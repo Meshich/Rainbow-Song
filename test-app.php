5c2a706c08490b2c6e84b241387144f2793bef0d
Meshich <maid91@hotmail.com>
2017-08-23 16:18:45 +0200

    New html file

---
+++ test-app.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+ <head>
+ <meta charset="utf-8">
+ <title>Example</title>
+ <script>
+document.write("hello");
+ </script>
+ </head>
+ <body>
+
+ </body>
+</html>
